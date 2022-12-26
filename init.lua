f82052dd577c0b50eb4f45cc137ebc5f5013d1d4
abdulkader-safi <safi.abdulkader@gmail.com>
2022-12-26 14:50:15 +0200

    new nv config

---
+++ init.lua
@@ -0,0 +1,3 @@
+require "safi"
+
+print("Hello Master Safi")
---
+++ lua/safi/remap.lua
@@ -0,0 +1,6 @@
+local g = vim.g
+local o = vim.o
+local opt = vim.opt
+
+g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
